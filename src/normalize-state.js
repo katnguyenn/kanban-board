e6386baff49987864f2d0bd763a20f567628a3f7
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-16 11:40:39 -0700

    combined Kanban repos from frontend masters to get user data in default-state

--- src/normalize-state.js
+++ src/normalize-state.js
@@ -11,17 +11,17 @@ const normalizedLists = normalize(defaultState.lists, [list]);
 const normalizedUsers = normalize(defaultState.users, [user]);
 
 export const lists = {
-    entities: normalizedLists.enetities.lists,
+    entities: normalizedLists.entities.lists,
     ids: normalizedLists.result
 };
 
 export const users = {
-    entities: normalizedUsers.enetities.users,
+    entities: normalizedUsers.entities.users,
     ids: normalizedUsers.result
 };
 
 export const cards = {
-    entities: normalizedLists.enetities.cards,
+    entities: normalizedLists.entities.cards,
     ids: Object.keys(normalizedLists.entities.cards)
 };
 
