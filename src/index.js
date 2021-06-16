78e7812f1b3b8562d241cee74264766094f13fb9
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-16 10:51:55 -0700

    initial project files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Application from "./Components/Application";
+
+import './index.scss';
+
+ReactDOM.render(<Application />, document.getElementById('root'));
